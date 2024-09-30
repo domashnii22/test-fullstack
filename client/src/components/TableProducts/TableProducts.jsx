986760eddee7268de5747435a07eb409cbda54c8
domashnii22 <domashnii.al@yandex.ru>
2024-09-30 10:37:25 +0300

    feat: add infinite loading

--- client/src/components/TableProducts/TableProducts.jsx
+++ client/src/components/TableProducts/TableProducts.jsx
@@ -1,27 +1,67 @@
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-quartz.css';
-import { useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 
-export default function TableProducts({ products }) {
-  const gridRef = useRef();
+export default function TableProducts() {
   const [columnDefs, setColumnDefs] = useState([
     { field: 'product_id', headerName: 'Id' },
     { field: 'product_name', headerName: 'Product' },
     {
       field: 'product_price',
       headerName: 'Price',
-      valueFormatter: p => '$' + p.value.toLocaleString(),
     },
     { field: 'product_acceptance_date', headerName: 'Acceptance date' },
   ]);
 
+  const limit = 5;
+
+  const onGridReady = useCallback(async params => {
+    let data = await fetch(
+      `http://localhost:3000/api/product?page=1&limit=${limit}`,
+    );
+
+    data = await data.json();
+
+    const dataSource = {
+      rowCount: undefined,
+      getRows: async params => {
+        const currentPageNumber = Math.floor(params.endRow / limit);
+        let lastRow = -1;
+        let list = data['products'][0];
+
+        if (currentPageNumber !== -1) {
+          let nextPageData = await fetch(
+            `http://localhost:3000/api/product?page=${currentPageNumber}&limit=${limit}`,
+          );
+          nextPageData = await nextPageData.json();
+
+          list = nextPageData['products'][0];
+        }
+
+        if (list?.length < limit) {
+          lastRow = params?.startRow + list?.length;
+        }
+
+        list?.length
+          ? params.successCallback(list, lastRow)
+          : params.failCallback();
+      },
+    };
+    params.api.setGridOption('datasource', dataSource);
+  }, []);
+
   return (
     <div
       className="ag-theme-quartz"
-      style={{ height: '65vh', width: 800, marginTop: 58 }}
+      style={{ height: 300, width: 800, marginTop: 58 }}
     >
-      <AgGridReact ref={gridRef} rowData={products} columnDefs={columnDefs} />
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowModelType="infinite"
+        onGridReady={onGridReady}
+        cacheBlockSize={limit}
+      />
     </div>
   );
 }
--- client/src/pages/Tables/Tables.jsx
+++ client/src/pages/Tables/Tables.jsx
@@ -2,43 +2,27 @@ import { CircularProgress, Stack } from '@mui/material';
 import { useEffect, useState } from 'react';
 import TableProducts from '../../components/TableProducts/TableProducts';
 import TableCategories from '../../components/TableCategories/TableCategories';
-import {
-  useGetCategoriesQuery,
-  useGetProductsQuery,
-} from '../../utils/MainApi';
+import { useGetCategoriesQuery } from '../../utils/MainApi';
 
 export default function Tables() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [currentLimit, setCurrentLimit] = useState(10);
-  const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
 
-  const { data: dataOfProducts, isLoading: productsIsLoading } =
-    useGetProductsQuery({
-      page: currentPage,
-      limit: currentLimit,
-    });
-
-  const { data: dataOfCategories, isLoading: categoriesIsLoading } =
-    useGetCategoriesQuery({
-      page: currentPage,
-      limit: 100,
-    });
-
-  const isLoading = productsIsLoading || categoriesIsLoading;
+  const { data, isLoading } = useGetCategoriesQuery({
+    page: 1,
+    limit: 100,
+  });
 
   useEffect(() => {
     if (!isLoading) {
-      setProducts(dataOfProducts['products'][0]);
-      setCategories(dataOfCategories['categories'][0]);
+      setCategories(data['categories'][0]);
     }
-  }, [isLoading, dataOfProducts, dataOfCategories]);
+  }, [isLoading, data]);
 
   return isLoading ? (
     <CircularProgress size="3rem" />
   ) : (
     <Stack direction="row" gap="40px">
-      <TableProducts products={products}></TableProducts>
+      <TableProducts></TableProducts>
       <TableCategories categories={categories}></TableCategories>
     </Stack>
   );
